d6e1ea4377cf304483f135a7cf4b79f81d8ec6ca
ibalram <balramsinghrathore3@gmail.com>
2020-12-14 16:59:51 +0530

    Added: Ability to display message with input

--- src/App.js
+++ src/App.js
@@ -7,20 +7,29 @@ class App extends React.Component{
   constructor(){
     super()
     this.state = {
-      title: 'Hello from Bridgelabz'
+      userName: ''
     }
   }
   onClick = ($event) => {
     console.log("save button is clicked!", $event);
     window.open(this.url, "_blank");
   }
+  onNameChange = (event) => {
+    console.log("value is ", event.target.value);
+    this.setState({userName: event.target.value})
+  }
   render(){
     return (
-      <div>
-        <h1>{this.state.title}</h1>
-        <img src={logo} onClick = {this.onClick}
-          alt="The Bridgelabz logo: a Bridge to Employment through lab works"></img>
-      </div>
+      <>
+        <div>
+          <h1>Hello {this.state.userName} from Bridgelabz</h1>
+          <img src={logo} onClick = {this.onClick}
+            alt="The Bridgelabz logo: a Bridge to Employment through lab works"></img>
+        </div>
+        <div>
+          <input onChange={this.onNameChange}></input>
+        </div>
+      </>
     );
   }
 }
