a3117eaf0a950bdc1b4dcc84d8fb27ad308572cc
ibalram <balramsinghrathore3@gmail.com>
2020-12-14 16:42:05 +0530

    Added: Ability to launch B site in tab by clicking

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import './App.css';
 import React from 'react';
 
 class App extends React.Component{
+  url = 'https://www.bridgelabz.com/'
   constructor(){
     super()
     this.state = {
       title: 'Hello from Bridgelabz'
     }
   }
+  onClick = ($event) => {
+    console.log("save button is clicked!", $event);
+    window.open(this.url, "_blank");
+  }
   render(){
     return (
       <div>
         <h1>{this.state.title}</h1>
-        <img src={logo} alt="The Bridgelabz logo: a Bridge to Employment through lab works"></img>
+        <img src={logo} onClick = {this.onClick}
+          alt="The Bridgelabz logo: a Bridge to Employment through lab works"></img>
       </div>
     );
   }
